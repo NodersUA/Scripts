c1faa65e0b4fc31cab7ca02806faf35a8645a3b1
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2023-03-30 21:26:11 +0200

    Update Andromeda.sh

--- Andromeda.sh
+++ Andromeda.sh
@@ -182,41 +182,37 @@ andromedad tx staking create-validator \
 break
 ;;
 "Synchronization via StateSync")
-echo -e "      \e[1m\e[32m SOOOON\e[0m"
-
-break
-;;
-"UPDATE")
-echo -e "      \e[1m\e[32m Update soon\e[0m"
-
-cd $HOME/andromedad
-git pull
-git checkout 
-make build
-$HOME/andromedad/build/andromedad version --long | grep -e version -e commit
-systemctl stop andromedad
-mv $HOME/andromedad/build/andromedad $(which andromedad)
-andromedad version --long | grep -e version -e commit
+SNAP_RPC=http://andromedad.rpc.t.stavr.tech:4137
+peers="247f3c2bed475978af238d97be68226c1f084180@andromedad.peer.stavr.tech:4376"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.andromedad/config/config.toml
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 100)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.andromedad/config/config.toml
+andromedad tendermint unsafe-reset-all --home $HOME/.andromedad
 systemctl restart andromedad && journalctl -u andromedad -f -o cat
 
 break
 ;;
+"UPDATE")
+echo -e "      \e[1m\e[32m SOOOON\e[0m"
+andromedad version
+
+break
+;;
 "Check node logs")
-journalctl -u andromedad -f -o cat
+sudo journalctl -u andromedad -f -o cat
 
 break
 ;;
 "Synchronization via SnapShot")
-echo -e "      \e[1m\e[32m SnapShot\e[0m"
-
-snapshot_interval=1000
-sed -i.bak -e "s/^snapshot-interval *=.*/snapshot-interval = \"$snapshot_interval\"/" ~/.andromedad/config/app.toml
-
-# install lz4
-apt update
-apt install snapd -y
-snap install lz4
-
+cd $HOME
+apt install lz4
 sudo systemctl stop andromedad
 cp $HOME/.andromedad/data/priv_validator_state.json $HOME/.andromedad/priv_validator_state.json.backup
 rm -rf $HOME/.andromedad/data
@@ -242,8 +238,6 @@ break
 ;;
 "Exit")
 exit
-break
 esac
 done
 done
-

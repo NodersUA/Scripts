32694e2d30e6387ba61405b429144d92219d9cd0
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2023-03-31 21:18:30 +0200

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-"        \e[1m\e[33m41. "NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA   " \e[0m"
+echo -e" \e[1m\e[33m41. "NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA   " \e[0m"
 echo "NNN   NNN  OO     OO  DD    DD  EE       RR   RR SS        UU      UU     AA AA     "
 echo "NNNN  NNN OO       OO DD     DD EE       RR   RR SS        UU      UU     AA  AA   "
 echo "NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA       "

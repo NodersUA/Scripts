a40db62a35468cc966f7365242a8eceb7d777c61
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2023-04-04 16:27:13 +0200

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-echo -e "\033[0;34m        " NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA "   
-                      echo " NNN   NNN  OO     OO  DD    DD  EE       RR   RR SS        UU      UU     AA AA "   
-                      echo " NNNN  NNN OO       OO DD     DD EE       RR   RR SS        UU      UU     AA  AA "   
-                      echo " NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA "  
-echo -e "\033[0;33m        " NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA "   
-                      echo " NNN NNNNN OO       OO DD     DD EE       RR  RR       SS   UU      UU    AA     AA "  
-                      echo " NNN  NNNN  OO     OO  DD    DD  EE       RR   RR      SS    UU    UU    AA       AA "  
-                      echo " NNN   NNN   OOOOOOO   DDDDDD    EEEEEEEE RR    RR SSSSS      UUUUUU    AA         AA "   
-echo -e \033[00m" 
+BLUE="\e[34m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+echo -e "${BLUE}   " NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA " ${ENDCOLOR}"   
+echo -e "${BLUE}   " NNN   NNN  OO     OO  DD    DD  EE       RR   RR SS        UU      UU     AA AA " ${ENDCOLOR}"  
+echo -e "${BLUE}   " NNNN  NNN OO       OO DD     DD EE       RR   RR SS        UU      UU     AA  AA " ${ENDCOLOR}"  
+echo -e "${BLUE}   " NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA " ${ENDCOLOR}" 
+echo -e "${YELLOW} " NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA "  ${ENDCOLOR}" 
+echo -e "${YELLOW} " NNN NNNNN OO       OO DD     DD EE       RR  RR       SS   UU      UU    AA     AA "  ${ENDCOLOR}"
+echo -e "${YELLOW} " NNN  NNNN  OO     OO  DD    DD  EE       RR   RR      SS    UU    UU    AA       AA "  ${ENDCOLOR}"
+echo -e "${YELLOW} " NNN   NNN   OOOOOOO   DDDDDD    EEEEEEEE RR    RR SSSSS      UUUUUU    AA         AA "  ${ENDCOLOR}" 
+
 

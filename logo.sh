09ee03d992330f184abcbfe7a300777d9d5e448c
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2023-04-04 16:32:15 +0200

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -4,13 +4,14 @@ BLUE="\e[34m"
 YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
 
-echo -e "${BLUE}    NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA        ${ENDCOLOR}"   
-echo -e "${BLUE}    NNN   NNN  OO     OO  DD    DD  EE       RR   RR SS        UU      UU     AA AA       ${ENDCOLOR}"  
-echo -e "${BLUE}    NNNN  NNN OO       OO DD     DD EE       RR   RR SS        UU      UU     AA  AA      ${ENDCOLOR}"  
-echo -e "${BLUE}    NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA     ${ENDCOLOR}" 
+>================>=============>========>==========>=======>==========>=========>=========>=========>=====
+echo -e "${BLUE}  NN    NNN    OOOOO    DDDDDD    EEEEEEEE RRRRRR   SSSSS    UU      UU      AAA        ${ENDCOLOR}"   
+echo -e "${BLUE}  NNN   NNN  OO     OO  DD    DD  EE       RR   RR SS        UU      UU     AA AA       ${ENDCOLOR}"  
+echo -e "${BLUE}  NNNN  NNN OO       OO DD     DD EE       RR   RR SS        UU      UU     AA  AA      ${ENDCOLOR}"  
+echo -e "${BLUE}  NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA     ${ENDCOLOR}" 
 echo -e "${YELLOW}  NNNNN NNN OO       OO DD     DD EEEEEE   RRRRRR   SSSSS    UU      UU    AAAAAAAA     ${ENDCOLOR}" 
 echo -e "${YELLOW}  NNN NNNNN OO       OO DD     DD EE       RR  RR       SS   UU      UU    AA     AA    ${ENDCOLOR}"
 echo -e "${YELLOW}  NNN  NNNN  OO     OO  DD    DD  EE       RR   RR      SS    UU    UU    AA       AA   ${ENDCOLOR}"
 echo -e "${YELLOW}  NNN   NNN   OOOOOOO   DDDDDD    EEEEEEEE RR    RR SSSSS      UUUUUU    AA         AA  ${ENDCOLOR}" 
-
+>================>=============>========>==========>=======>==========>=========>=========>=========>=====
 

d65d889f75605776737d51051697f1232ab4815b
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-26 20:10:56 +0300

    Update node_specific_sample.sh

--- cosmos/node_specific_sample.sh
+++ cosmos/node_specific_sample.sh
@@ -5,6 +5,9 @@ NODE_CHAIN_ID="cascadia_6102-1"
 NODE_PORT="18"
 BINARY_VERSION_TAG="v0.1.2"
 CHAIN_DENOM="aCC"
+BINARY_NAME="cascadiad"
+DIRECTORY="cascadia"
+HIDDEN_DIRECTORY=".cascadiad"
 # *************************************
 
 # export temp variables
@@ -13,6 +16,9 @@ echo 'export NODE_CHAIN_ID='$NODE_CHAIN_ID >> $HOME/config.sh
 echo 'export NODE_PORT='$NODE_PORT >> $HOME/config.sh
 echo 'export BINARY_VERSION_TAG='$BINARY_VERSION_TAG >> $HOME/config.sh
 echo 'export CHAIN_DENOM='$CHAIN_DENOM >> $HOME/config.sh
+echo 'export BINARY_NAME='$BINARY_NAME >> $HOME/config.sh
+echo 'export DIRECTORY='$DIRECTORY >> $HOME/config.sh
+echo 'export HIDDEN_DIRECTORY='$HIDDEN_DIRECTORY >> $HOME/config.sh
 source $HOME/config.sh
 
 # import code from cosmos/install.sh

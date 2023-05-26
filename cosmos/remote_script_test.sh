f9cc8b25308ab7744709502398e516252d7e367a
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-26 20:02:55 +0300

    Delete remote_script_test.sh

--- cosmos/remote_script_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "*****************************"
-echo -e "\e[1m\e[32m Chain id:           $NODE_CHAIN_ID \e[0m"
-echo -e "\e[1m\e[32m Chain demon:        $CHAIN_DENOM \e[0m"
-echo -e "\e[1m\e[32m Node port:        $NODE_PORT \e[0m"
-echo -e "\e[1m\e[32m Binary version tag: $BINARY_VERSION_TAG \e[0m"
-echo -e "\e[1m\e[32m NIBIRU MONIKER FROM BASHPROFILE: $NIBIRU_MONIKER \e[0m"
-echo "*****************************"

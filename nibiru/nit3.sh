da7551006d8719cb3eca7d1226261389e396cab7
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-31 21:28:38 +0300

    Update nit3.sh

--- nibiru/nit3.sh
+++ nibiru/nit3.sh
@@ -6,12 +6,12 @@ percentage_max=$(( (number * 30) / 100 ))
 
 # Open
 # nibid tx perp open-position [buy/sell] [pair] [leverage] [quoteAmt / sdk.Dec] [baseAmtLimit / sdk.Dec] [flags]
-#command=$"nibid tx perp open-position buy ubtc:unusd $(shuf -i 10-100 -n 1 | awk '{printf "%.1f", $0/10}') $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
-command="nibid tx perp open-position buy ubtc:unusd $(shuf -i 10-100 -n 1 | awk '{printf "%.1f", $0/10}') $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+#command="nibid tx perp open-position buy ubtc:unusd $(shuf -i 10-100 -n 1 | awk '{printf "%.1f", $0/10}') $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+command="nibid tx perp open-position buy ubtc:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
 echo $command && eval $command
 
 sleep $(shuf -i 30-200 -n 1)
-command=$"nibid tx perp open-position buy ueth:unusd $(shuf -i 10-100 -n 1 | awk '{printf "%.1f", $0/10}') $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+command=$"nibid tx perp open-position buy ueth:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
 echo $command && eval $command
 
 # sleep 30-120 min

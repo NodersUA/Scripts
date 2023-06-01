6c58d182ec8be60b7073dc6f61da433337f9ecdb
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-06-01 16:10:37 +0300

    Update nit3.sh

--- nibiru/nit3.sh
+++ nibiru/nit3.sh
@@ -7,28 +7,29 @@ percentage_max=$(( (number * 30) / 100 ))
 # Open
 # nibid tx perp open-position [buy/sell] [pair] [leverage] [quoteAmt / sdk.Dec] [baseAmtLimit / sdk.Dec] [flags]
 #command="nibid tx perp open-position buy ubtc:unusd $(shuf -i 10-100 -n 1 | awk '{printf "%.1f", $0/10}') $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
-command="nibid tx perp open-position buy ubtc:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+##command="nibid tx perp open-position buy ubtc:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+command="nibid tx perp open-position buy ubtc:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --fees=8500unibi -y"
 echo $command && eval $command
 
 sleep $(shuf -i 30-200 -n 1)
-command=$"nibid tx perp open-position buy ueth:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y"
+command=$"nibid tx perp open-position buy ueth:unusd $(echo "scale=1; $(shuf -i 10-100 -n 1) / 10" | bc) $(shuf -i $percentage_min-$percentage_max -n 1)000 0 --from wallet --fees=8500unibi -y"
 echo $command && eval $command
 
 # sleep 30-120 min
 sleep $(shuf -i 1800-7200 -n 1)
 
 # Close
-nibid tx perp close-position ubtc:unusd  --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx perp close-position ubtc:unusd  --from wallet --fees=8500unibi -y
 sleep $(shuf -i 30-200 -n 1)
-nibid tx perp close-position ueth:unusd  --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx perp close-position ueth:unusd  --from wallet --fees=8500unibi -y
 sleep $(shuf -i 100-300 -n 1)
 
 # Swap
-nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unusd --token-out-denom=uusdt --from wallet --pool-id=2 --chain-id=$NIBIRU_CHAIN_ID --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unusd --token-out-denom=uusdt --from wallet --pool-id=2 --chain-id=$NIBIRU_CHAIN_ID --fees=8500unibi -y
 sleep $(shuf -i 30-200 -n 1)
-nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000uusdt --token-out-denom=unusd --from wallet --pool-id=2 --chain-id=$NIBIRU_CHAIN_ID --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000uusdt --token-out-denom=unusd --from wallet --pool-id=2 --chain-id=$NIBIRU_CHAIN_ID --fees=8500unibi -y
 sleep $(shuf -i 30-200 -n 1)
 
-nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unusd --token-out-denom=unibi --from wallet --pool-id=1 --chain-id=$NIBIRU_CHAIN_ID --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unusd --token-out-denom=unibi --from wallet --pool-id=1 --chain-id=$NIBIRU_CHAIN_ID --fees=8500unibi -y
 sleep $(shuf -i 30-200 -n 1)
-nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unibi --token-out-denom=unusd --from wallet --pool-id=1 --chain-id=$NIBIRU_CHAIN_ID --gas-adjustment 1.4 --gas auto --gas-prices 0.025unibi -y
+nibid tx spot swap-assets --token-in=$(shuf -i $percentage_min-$percentage_max -n 1)000unibi --token-out-denom=unusd --from wallet --pool-id=1 --chain-id=$NIBIRU_CHAIN_ID --fees=8500unibi -y

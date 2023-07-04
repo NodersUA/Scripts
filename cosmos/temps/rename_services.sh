628dc4f3a7dce0de75195c3b13b3f1e7902049e1
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-07-04 14:40:10 +0300

    Update rename_services.sh

--- cosmos/temps/rename_services.sh
+++ cosmos/temps/rename_services.sh
@@ -4,3 +4,5 @@
 echo "8" | source <(curl -s https://raw.githubusercontent.com/NodersUA/Scripts/main/defund)
 # update autorestart Nibiru
 echo "8" | source <(curl -s https://raw.githubusercontent.com/NodersUA/Scripts/main/nibiru/nibiru)
+# update autorestart Cascadia
+echo "8" | source <(curl -s https://raw.githubusercontent.com/NodersUA/Scripts/main/cascadia)

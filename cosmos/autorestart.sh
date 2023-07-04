e7b39cfef446ec69da038c4edd9d24752d9a0311
MYKHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-07-04 14:53:05 +0300

    Update autorestart.sh

--- cosmos/autorestart.sh
+++ cosmos/autorestart.sh
@@ -8,8 +8,7 @@ echo "BINARY_NAME: $BINARY_NAME"
 # make sure scripts is in path. If not then make dir
 mkdir -p "$HOME/scripts"
 
-# write nibiru_autorestart.sh to scripts/
-tee $HOME/scripts/${DIRECTORY}_autorestart.sh > /dev/null <<EOF
+tee $HOME/scripts/ar_${DIRECTORY}.sh > /dev/null <<EOF
 #!/bin/sh
 
 while true; do
@@ -32,19 +31,19 @@ done
 EOF
 
 # set run script rights
-chmod +x $HOME/scripts/${DIRECTORY}_autorestart.sh
+chmod +x $HOME/scripts/ar_${DIRECTORY}.sh
 # avoid error <BINARY_NAME> command not found
 sudo cp $HOME/go/bin/$BINARY_NAME /usr/local/bin/
 
 # Create <DIRECTORY>_autorestart service file (One command)
-sudo tee /etc/systemd/system/${DIRECTORY}_autorestart.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/ar_${DIRECTORY}.service > /dev/null <<EOF
 [Unit]
 Description=$DIRECTORY Autorestart Service
 After=network.target
 
 [Service]
 User=$USER
-ExecStart=$HOME/scripts/${DIRECTORY}_autorestart.sh
+ExecStart=$HOME/scripts/ar_${DIRECTORY}.sh
 Restart=always
 
 [Install]
@@ -53,6 +52,6 @@ EOF
 
 # Start <DIRECTORY>_autorestart service file
 systemctl daemon-reload
-# systemctl enable ${DIRECTORY}_autorestart.service
-# systemctl restart ${DIRECTORY}_autorestart.service
+# systemctl enable ar_${DIRECTORY}.service
+# systemctl restart ar_${DIRECTORY}.service
 

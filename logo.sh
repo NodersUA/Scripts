41be2c77ccb7310f5504f7cf9fc795c7029690c9
MYKHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-07-14 21:19:53 +0300

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BLUE="\e[34m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+echo ">================>=============>========>==========>=======>==========>=========>=========>=========>====="
+echo -e "${BLUE}   UU     UU         A           NNN     NN    OOOO    DDDDDDD    EEEEEEEEEE  SSSSSSSS    ${ENDCOLOR}"   
+echo -e "${BLUE}   UU     UU        AAA          NNNN    NN  OO    OO  DD     DD  EE         SS           ${ENDCOLOR}"  
+echo -e "${BLUE}   UU     UU       AA AA         NN NN   NN OO      OO DD      DD EE         SS           ${ENDCOLOR}"  
+echo -e "${BLUE}   UU     UU      AA   AA        NN  NN  NN OO      OO DD      DD EEEEEEE     SSSSSS      ${ENDCOLOR}" 
+echo -e "${YELLOW}   UU     UU     AA     AA       NN   NN NN OO      OO DD      DD EEEEEEE      SSSSSSS    ${ENDCOLOR}" 
+echo -e "${YELLOW}   UU     UU    AAAAAAAAAAA      NN    NNNN OO      OO DD      DD EE                 SS   ${ENDCOLOR}"
+echo -e "${YELLOW}    UU   UU    AA         AA     NN     NNN  OO    OO  DD     DD  EE                 SS   ${ENDCOLOR}"
+echo -e "${YELLOW}     UUUUU    AA           AA    NN      NN   OOOOOO   DDDDDDD    EEEEEEEEEE  SSSSSSSS    ${ENDCOLOR}" 
+echo ">================>=============>========>==========>=======>==========>=========>=========>=========>====="

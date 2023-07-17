20a985a612cbb3d08f6a4f6c00f38d46fbc247d9
MYKHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-07-18 02:43:36 +0300

    Update autodelegate.sh

--- cosmos/autodelegate.sh
+++ cosmos/autodelegate.sh
@@ -100,6 +100,3 @@ EOF
 systemctl daemon-reload
 systemctl enable ad_${DIRECTORY}
 systemctl restart ad_${DIRECTORY}
-
-echo '=============== SETUP FINISHED ==================='
-echo -e 'To check logs:        \e[1m\e[33mjournalctl -u ad_${DIRECTORY} -f -o cat\e[0m'

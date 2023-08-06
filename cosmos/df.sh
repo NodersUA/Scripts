823bb978a2ed73ccef9e2a33612bdcf1a9e0595b
MYKHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-08-06 03:19:38 +0300

    Update df.sh

--- cosmos/df.sh
+++ cosmos/df.sh
@@ -5,7 +5,7 @@ if [ -d "$DISKORD_DIR" ]; then
     rm -rf "$DISKORD_DIR"
 fi
 
-if [ "$DISCORD_DIR" == "df_cascadia" ]; then
+if [ "$DISCORD_DIR" == "df_cascadia" ] && [ -d "$CASCADIA_TOKEN" ]; then
   TOKEN=$CASCADIA_TOKEN
 fi
 
